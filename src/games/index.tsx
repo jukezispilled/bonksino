62a57d9925cc4fac4ed1ede456271665da8ccd6e
agorapilled <johngardner13@outlook.com>
2025-04-25 22:50:02 -0400

    v0

--- src/games/index.tsx
+++ src/games/index.tsx
@@ -80,7 +80,6 @@ export const GAMES: GameBundle[] = [
       name: 'Plinko',
       description: `
         Plinko is played by dropping chips down a pegged board where they randomly fall into slots with varying win amounts. Each drop is a mix of anticipation and strategy, making Plinko an endlessly entertaining game of chance.
-        ⚠️ Under development. Results shown might be incorrect. ⚠️
       `,
     },
     app: React.lazy(() => import('./Plinko')),
